ebfc725356e6121a0c4a66ce9636eae60601dcc4
Ornella RAZAFINONY ROBINSON <orazafin@e2r6p12.42.fr>
2017-03-07 18:02:27 +0100

    GNL_07031802

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: orazafin <orazafin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/21 19:16:10 by orazafin          #+#    #+#             */
+/*   Updated: 2017/03/07 11:39:12 by orazafin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE
+# define GET_NEXT_LINE
+
+# define BUFF_SIZE 50
+#include "Libft/Libft.h"
+#include <unistd.h>
+#include <stdlib.h>
+
+#endif
