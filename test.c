cc956f9f8e9b1e9ac24b2a7b26101bc3e465fc7c
Ornella RAZAFINONY ROBINSON <orazafin@e1r4p6.42.fr>
2017-03-09 16:02:20 +0100

    gnl_09031602

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: orazafin <orazafin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/08 10:40:17 by orazafin          #+#    #+#             */
-/*   Updated: 2017/03/08 15:10:36 by orazafin         ###   ########.fr       */
+/*   Updated: 2017/03/09 15:03:28 by orazafin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,37 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#define BUFF_SIZE 2
 
-#define BUFF_SIZE 10
+size_t	ft_strlen(const char *str)
+{
+	size_t i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int		i;
+	char	*str;
+	int		end;
+
+	i = 0;
+	str = (char *)s;
+	end = ft_strlen(s);
+	if (c == '\0')
+		return (&str[end]);
+	while ((char)str[i] != '\0')
+	{
+		if ((char)str[i] == c)
+			return ((char *)(str + i));
+		i++;
+	}
+	return (0);
+}
 
 int		open_file(char const *file)
 {
@@ -27,31 +56,29 @@ int		open_file(char const *file)
 	return (fd);
 }
 
-int get_next_line(int fd, char **line)
+int		get_next_line(int fd, char **line)
 {
-	int 		read_octet;
-	static int	i = 0;
-
-	read_octet = read_file(fd);
-	while (i < read_octet)
-	{
-
-	}
-	return (0);
-}
-
-int		read_file(int fd)
-{
-	char *buff;
+	static char *str = NULL;
+	char buff[BUFF_SIZE + 1];
 	int value;
 	int read_octet;
+	char *tmp;
 
+	tmp = NULL;
 	value = 0;
-	read_octet = 0;
-	if (!(buff = malloc(sizeof(char *) * BUFF_SIZE + 1)))
-		return (-1);
-	while ((read_octet = read(fd, buff, BUFF_SIZE)) != 0)
+	while ((read_octet = read(fd, buff, BUFF_SIZE)) != 0 && (ft_strchr((const char *)buff, '\n') == 0) && (*line = buff))
+	{
+		printf("%s\n", buff);
+		printf("%p\n", *line);
+		tmp = ft_strjoin((char const *)str, (char const *)buff);
+		free(str);
+		str = tmp;
+		printf("%s\n", str);
 		value += read_octet;
+		printf("%d\n", value);
+		printf("\n");
+	}
+
 	return (value);
 }
 
@@ -59,9 +86,10 @@ int		main(int argc, char const *argv[])
 {
 	int fd;
 	int value;
+	char *line;
 
 	if ((fd = open_file(argv[1])) == -1)
 		return (-1);
-	value = read_file(fd);
+	value = get_next_line(fd, &line);
 	return (0);
 }
