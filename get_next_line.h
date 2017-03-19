746153ddfccb46375e0e41ed29d053b95341a9f9
ROBINSON Ornella <robinson@MacBook-Pro-Ornella.local>
2017-03-20 00:56:36 +0100

    GNL_20030056

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: orazafin <orazafin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/21 19:16:10 by orazafin          #+#    #+#             */
-/*   Updated: 2017/03/14 16:49:45 by orazafin         ###   ########.fr       */
+/*   Updated: 2017/03/19 17:19:48 by orazafin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # define GET_NEXT_LINE
 # define BUFF_SIZE 3
 
-#include "../Libft/Libft.h"
+#include "Libft/libft.h"
 #include <unistd.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -26,5 +26,4 @@
 #include <stdlib.h>
 
 int		get_next_line(int fd, char **line);
-
 #endif
--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: orazafin <orazafin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/08 10:40:17 by orazafin          #+#    #+#             */
-/*   Updated: 2017/03/14 18:02:19 by orazafin         ###   ########.fr       */
+/*   Updated: 2017/03/19 18:37:06 by orazafin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,27 @@ int		open_file(char const *file)
 {
 	int fd;
 
-	fd = open(file, O_RDONLY);
+	fd = open("/Users/robinson/Documents/Projects_42/Get_Next_Line/file_text.txt", O_RDONLY);
 	return (fd);
 }
-//
-// int		end_read(char *buff, static char *str, int read_octet)
-// {
-// 	char *tmp;
-// 	char *line_break;
-//
-// 	if (read_octet != 0)
-// 	{
-// 		buff[read_octet] = '\0';
-// 		line_break = ft_strchr((char const *)str, '\n');
-// 		str[line_break] = '\0';
-// 		*line = ft_strdup((const char *)str);
-// 		if (read_octet != 0)
-// 			return (1);
-// 	}
-// 	return (0);
-// }
+
+int		end_read(char *str, char *tmp, char *buff, char **line)
+{
+	char *end_line;
+
+	end_line = ft_strchr((const char *)buff, '\n');
+    if (end_line != 0)
+    {
+        tmp = str;
+        str = ft_strdup(end_line + 1);
+        *end_line = 0;
+        *line = ft_strjoin((char const *) tmp, (char const *) end_line);
+        free(tmp);
+        printf("%s\n", str);
+        return (1);
+    }
+    return (0);
+}
 
 int		get_next_line(int fd, char **line)
 {
@@ -43,30 +44,23 @@ int		get_next_line(int fd, char **line)
 	char buff[BUFF_SIZE + 1];
 	int read_octet;
 	char *tmp;
-	char *tmp1;
 
 	if (!fd)
 		return (-1);
-	while ((read_octet = read(fd, buff, BUFF_SIZE)) != 0
-	&& (tmp1 = ft_strchr((const char *)buff, '\n')) == 0)
+	while ((read_octet = read(fd, buff, BUFF_SIZE) != 0)
+	&& (ft_strchr((const char *)buff, '\n')) == 0)
 	{
 		buff[read_octet] = '\0';
 		if (!str)
 			str = ft_strdup(buff);
 		else
 		{
-			tmp = ft_strjoin((char const *)str, (char const *)buff);
+			tmp = ft_strjoin(str, buff);
 			free(str);
 			str = tmp;
 		}
 	}
-	tmp = str;
-	str = ft_strdup(tmp1 + 1);
-	*tmp1 = 0;
-	*line = ft_strjoin(tmp, tmp1);
-	free(tmp);
-	printf("%s\n", str);
-	return (0);
+	return (end_read(str, tmp, buff, line));
 }
 
 int		main(int argc, char const *argv[])
@@ -78,5 +72,10 @@ int		main(int argc, char const *argv[])
 	if ((fd = open_file(argv[1])) == -1)
 		return (-1);
 	value = get_next_line(fd, &line);
+	printf("%d\n", value);
+	value = get_next_line(fd, &line);
+	printf("%d\n", value);
+	value = get_next_line(fd, &line);
+	printf("%d\n", value);
 	return (0);
 }
