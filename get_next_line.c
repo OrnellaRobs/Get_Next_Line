a373f4051be7bc969fb0510ec0cb04654f1c217e
ROBINSON Ornella <robinson@192.168.1.102>
2017-04-03 17:36:45 +0100

    GNL_03041736

--- get_next_line.c
+++ get_next_line.c
@@ -5,76 +5,65 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: orazafin <orazafin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/03/08 10:40:17 by orazafin          #+#    #+#             */
-/*   Updated: 2017/03/29 09:34:56 by orazafin         ###   ########.fr       */
+/*   Created: 2017/03/30 16:44:30 by orazafin          #+#    #+#             */
+/*   Updated: 2017/04/03 12:25:39 by orazafin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int		line_break(char **str, char *tmp, char *buff, char **line)
+int         linebreak(char **str, char *buff, char **line, char *line_break)
 {
-	char *end_line;
+	char *eof;
+	char *save;
 
-	tmp = *str;
-	end_line = ft_strchr((const char *)buff, '\n');
-	*str = ft_strdup(end_line + 1);
-	*end_line = '\0';
-	*line = ft_strjoin((char const *)tmp, (char const *)buff);
-	printf("--------------------> *line = %s\n", *line);
-	// free(tmp);
+	save = *str;
+	if (!line_break && (eof = ft_strchr(buff, '\n')))
+	{
+		if (*(eof + 1))
+			*str = ft_strdup(eof + 1);
+		else
+			*str = "";
+		*eof = '\0';
+		*line = ft_strjoin(save, buff);
+		return (1);
+	}
+	else if (line_break)
+	{
+		*str = ft_strdup(line_break + 1);
+		*line_break = '\0';
+		*line = ft_strdup(save);
+		return (1);
+	}
+	*str = "";
+	*line = ft_strdup(save);
 	return (1);
 }
 
-int		end_reading(char **str, char **line)
+int	get_next_line(int fd, char **line)
 {
-	char *end_line;
-	char *cpy;
-	int	count_char;
-
-	end_line = ft_strchr((const char *)*str, '\n');
-	if (end_line != NULL && *(end_line +1))
-	{
-		count_char = ft_strlen(*str) - ft_strlen(end_line);
-		if (!(cpy = malloc(sizeof(char) * count_char + 1)))
-			return -1;
-		cpy = ft_strncpy(cpy, *str, count_char);
-		*str = ft_strdup(end_line + 1);
-		*line = ft_strcpy(*line, cpy);
-		printf("--------------------> *line = %s\n", *line);
-		free(cpy);
-		return (1);
-	}
-	*line = ft_strdup(*str);
-	printf("str = %s\n", *str);
-	free(*str);
-	printf("str = %s\n", *str);
-	printf("--------------------> *line = %s\n", *line);
-	return (0);
-}
-
-int		get_next_line(int fd, char **line)
-{
-	static char *str = NULL;
+    static char *str = "";
 	char buff[BUFF_SIZE + 1];
-	int read_octet;
-	char *tmp;
+	ssize_t read_octet;
+	char	*line_break;
+	char	*tmp = NULL;
 
-	if (!line || fd < 0 || BUFF_SIZE <= 0)
+	read_octet = 0;
+	if (!(line) || fd < 0 || BUFF_SIZE <= 0)
 		return (-1);
-	while ((read_octet = read(fd, buff, BUFF_SIZE)) != 0)
+	while (!(line_break = ft_strchr(str, '\n')) && (read_octet = read(fd, buff, BUFF_SIZE)))
 	{
+		if (read_octet == -1)
+			return (-1);
 		buff[read_octet] = '\0';
-		if (ft_strchr((const char *)buff, '\n') != 0)
-			return (line_break(&str, tmp, buff, line));
-		if (!str)
-			str = ft_strdup(buff);
-		else
-		{
-			tmp = ft_strjoin(str, buff);
-			free(str);
-			str = tmp;
-		}
+		if (ft_strchr(buff, '\n'))
+			return (linebreak(&str, buff, line, line_break));
+		tmp = ft_strjoin(str, buff);
+		str = tmp;
 	}
-	return (end_reading(&str, line));
+	if (read_octet == 0 && (ft_strncmp(str, "", 1)))
+		return(linebreak(&str, buff, line, line_break));
+	if (read_octet == 0)
+		*line = "";
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,30 +1,18 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: orazafin <orazafin@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/02/21 19:16:10 by orazafin          #+#    #+#             */
-/*   Updated: 2017/03/29 09:35:11 by orazafin         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
 
-//NE PAS OUBLIER D'ENLEVER LA LIBRAIRIE stdio!!!!!!!!!1
+//NE PAS OUBLIER D'ENLEVER LA LIBRAIRIE stdio + LES LIB DE OPEN ET DE READ !!!!!!!
 
-#ifndef GET_NEXT_LINE
-# define GET_NEXT_LINE
-# define BUFF_SIZE 2
-
-#include <stdio.h>
+# define BUFF_SIZE 8
+//#include <stdio.h>
 #include "Libft/libft.h"
 #include <unistd.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+//#include <sys/types.h>
+//#include <sys/stat.h>
+//#include <fcntl.h>
 #include <stdlib.h>
 
-int		main(int argc, char const *argv[]);
+//int		main(int argc, char const *argv[]);
 int		get_next_line(int fd, char **line);
+
 #endif
