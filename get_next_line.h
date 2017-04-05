979fe8affc84eb35a0fd3d3ca945d1926d24bcc2
OrnellaRobs <orazafin@student.42.fr>
2017-04-05 20:16:39 +0200

    gnl

--- get_next_line.h
+++ get_next_line.h
@@ -1,24 +1,18 @@
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
-
-//NE PAS OUBLIER D'ENLEVER LA LIBRAIRIE stdio + LES LIB DE OPEN ET DE READ !!!!!!!
+# define GET_NEXT_LINE_H
 
 # define BUFF_SIZE 8
-//#include <stdio.h>
-#include "Libft/libft.h"
-#include <unistd.h>
-//#include <sys/types.h>
-//#include <sys/stat.h>
-//#include <fcntl.h>
-#include <stdlib.h>
+# include "Libft/libft.h"
+# include <unistd.h>
+# include <stdlib.h>
 
-//int     main(int argc, char *argv[]);
 int		get_next_line(int fd, char **line);
 
 typedef struct      s_file
 {
-    int             fd;
-    char            *str;
-    struct s_file   *next;
+	int             fd;
+	char            *str;
+	struct s_file   *next;
 }                   t_file;
+
 #endif
