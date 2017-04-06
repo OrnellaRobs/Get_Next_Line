031c3b7e84fa8a7117fbb87c29ff78615959307e
OrnellaRobs <orazafin@e1r2p2.42.fr>
2017-04-06 16:02:35 +0200

    Get_Next_Line_Norme

--- get_next_line.h
+++ get_next_line.h
@@ -1,18 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: orazafin <orazafin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/06 15:37:26 by orazafin          #+#    #+#             */
+/*   Updated: 2017/04/06 15:43:43 by orazafin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
 # define BUFF_SIZE 8
-# include "Libft/libft.h"
+# include "./Libft/libft.h"
 # include <unistd.h>
 # include <stdlib.h>
 
-int		get_next_line(int fd, char **line);
-
-typedef struct      s_file
+typedef struct		s_file
 {
-	int             fd;
-	char            *str;
-	struct s_file   *next;
-}                   t_file;
+	int				fd;
+	char			*str;
+	struct s_file	*next;
+}					t_file;
 
+int					get_next_line(int fd, char **line);
 #endif
